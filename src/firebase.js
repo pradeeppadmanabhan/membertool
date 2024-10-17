55bbaefc739cf719c88d4f15a144e77633df1337
pradeeppadmanabhan <mailme.pradeep@gmail.com>
2024-10-17 11:31:07 +0530

    Updated firebase parameters obfuscataion

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-//import REACT_APP_FIREBASE_API_KEY from "./.env";
+import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "membertool-f2bcb.firebaseapp.com",
-  databaseURL:
-    "https://membertool-f2bcb-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "membertool-f2bcb",
-  storageBucket: "membertool-f2bcb.appspot.com",
-  messagingSenderId: "290989094157",
-  appId: "1:290989094157:web:4635f84b10b6162f0f6c44",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
+//console.log("FirebaseConfig :", firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
 
-export { database };
+const database = getDatabase(app);
+const storage = getStorage(app);
+
+export { storage, database };
