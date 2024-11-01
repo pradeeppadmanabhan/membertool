ea000cfae32eb0c79966643e2e3e97f6046cd804
pradeeppadmanabhan <mailme.pradeep@gmail.com>
2024-11-01 12:59:57 +0530

    Setup EMAILJS service for renewal reminder emails

---
+++ src/utils/SendEmail.js
@@ -0,0 +1,27 @@
+// src/utils/SendEmail.js
+import emailjs from "@emailjs/browser";
+
+const YOUR_SERVICE_ID = process.env.REACT_APP_EMAIL_SERVICE_ID;
+const YOUR_TEMPLATE_ID = process.env.REACT_APP_EMAIL_TEMPLATE_ID;
+const YOUR_PUBLIC_KEY = process.env.REACT_APP_EMAIL_PUBLIC_KEY;
+
+const sendEmail = async (templateParams) => {
+  try {
+    const response = await emailjs.send(
+      YOUR_SERVICE_ID,
+      YOUR_TEMPLATE_ID,
+      templateParams,
+      {
+        publicKey: YOUR_PUBLIC_KEY,
+      }
+    );
+
+    console.log("Email sent successfully:", response);
+    return true;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return false;
+  }
+};
+
+export default sendEmail;
