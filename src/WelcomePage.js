e32c4771298c1cad209fb6d8e5bb58062ef5add0
pradeeppadmanabhan <mailme.pradeep@gmail.com>
2025-05-29 12:16:29 +0530

    Cosmetic changes to the welcome page along with a note that site is best viewed on desktop/laptop.

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -10,11 +10,40 @@ const WelcomePage = () => {
     <div className="mt-5">
       <div className="content">
         <img src={logo} alt="KMA Logo" className="logo-image" />
-        <h1>Welcome to the Karnataka Mountaineering Association</h1>
+        <h2>Welcome to</h2>
+        <h1>The Karnataka Mountaineering Association</h1>
         <p>Your gateway to adventure, fitness, and the majestic mountains.</p>
         <div>
           <button onClick={signInWithGoogle}>Sign in with Google</button>
         </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="footer">
+        <p>
+          <i>
+            Best viewed in a modern browser like Chrome, Firefox, or Edge on
+            Laptop or Desktop.
+          </i>
+        </p>
+        {/* <p>
+          By signing in, you agree to our{" "}
+          <a href="/terms-of-service">Terms of Service</a> and{" "}
+          <a href="/privacy-policy">Privacy Policy</a>.
+        </p> */}
+        <p>
+          For any issues, please contact us at{" "}
+          <a href="mailto:info@kmaindia.org">info@kmaindia.org</a>.
+        </p>
+        <p>
+          © {new Date().getFullYear()} Karnataka Mountaineering Association. All
+          rights reserved.
+        </p>
+        <p>
+          <a href="https://kmaindia.org">Visit our website</a>
+        </p>
       </div>
     </div>
   );
